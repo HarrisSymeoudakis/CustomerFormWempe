29928b95c9dfe63d9801f2cc571a745607f083b1
Harris Symeoudakis <harris.symeoudakis@gmail.com>
2024-07-22 17:09:43 +0300

    Update general-information.js

--- general-information.js
+++ general-information.js
@@ -1,4 +1,4 @@
-fetch('https://ls-customerserver.onrender.com/latest')
+fetch('https://customerinfowempeserver.onrender.com/latest')
 	.then(response => response.json())
 	.then(data => {
 		console.log(data);
