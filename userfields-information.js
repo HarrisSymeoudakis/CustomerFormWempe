066cb9ae4334499f246f5dea168832a7128a0ec0
Harris Symeoudakis <harris.symeoudakis@gmail.com>
2024-07-23 14:29:34 +0300

    Update userfields-information.js

--- userfields-information.js
+++ userfields-information.js
@@ -4,14 +4,24 @@ fetch('https://customerinfowempeserver.onrender.com/swagger/CustomerUserFields')
 		console.log(data);
 		responseData = data;
 		const usrFields = data.userFields
-		// document.getElementById('usrfieldConnectedSalesperson').value = usrFields[0].value.listElement.value + " - " + usrFields[0].value.listElement.description || "";
-		// document.getElementById('usrfieldMiddleName').value = usrFields[1].value.text || "";
-		// document.getElementById('usrfieldSalutation').value = usrFields[2].value.text || "";
-		// document.getElementById('usrfieldAcademicTitile').value = usrFields[3].value.text || "";
-		// document.getElementById('usrfieldNobilityTitle').value = usrFields[4].value.text || "";
-		// document.getElementById('usrfieldNameAddition').value = usrFields[5].value.text || "";
-		// document.getElementById('usrfieldProfession').value = usrFields[6].value.text || "";
-		// document.getElementById('usrfieldClosestStore').value = usrFields[7].value.text || "";
+		document.getElementById('usrfield1').checked  = usrFields[0].value.boolean;
+		document.getElementById('usrfield2').checked  = usrFields[1].value.boolean;
+
+		document.getElementById('usrfield3').value = usrFields[2].value.number || "";
+		document.getElementById('usrfield4').value = usrFields[3].value.number || "";
+		document.getElementById('usrfield5').value = usrFields[5].value.number || "";
+		document.getElementById('usrfield6').value = usrFields[6].value.number || "";
+		document.getElementById('usrfield7').value = usrFields[7].value.number || "";
+		document.getElementById('usrfield8').value = usrFields[8].value.number || "";
+
+		document.getElementById('usrfield9').checked  = usrFields[9].value.boolean ;
+		document.getElementById('usrfield10').checked  = usrFields[10].value.boolean;
+		document.getElementById('usrfield11').checked  = usrFields[11].value.boolean ;
+		document.getElementById('usrfield12').checked  = usrFields[12].value.boolean ;
+		document.getElementById('usrfield13').checked  = usrFields[13].value.boolean ;
+		document.getElementById('usrfield14').checked  = usrFields[14].value.boolean;
+		
+		
 
 	})
 	.catch(error => console.error('Error fetching data:', error));
