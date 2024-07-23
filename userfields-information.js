806c155dd283067a536096c954e399a2d142a308
Harris Symeoudakis <harris.symeoudakis@gmail.com>
2024-07-23 14:33:13 +0300

    Update userfields-information.js

--- userfields-information.js
+++ userfields-information.js
@@ -7,12 +7,12 @@ fetch('https://customerinfowempeserver.onrender.com/swagger/CustomerUserFields')
 		document.getElementById('usrfield1').checked  = usrFields[0].value.boolean;
 		document.getElementById('usrfield2').checked  = usrFields[1].value.boolean;
 
-		document.getElementById('usrfield3').value = usrFields[2].value.number || "";
-		document.getElementById('usrfield4').value = usrFields[3].value.number || "";
-		document.getElementById('usrfield5').value = usrFields[5].value.number || "";
-		document.getElementById('usrfield6').value = usrFields[6].value.number || "";
-		document.getElementById('usrfield7').value = usrFields[7].value.number || "";
-		document.getElementById('usrfield8').value = usrFields[8].value.number || "";
+		document.getElementById('usrfield3').value = usrFields[2].value.number.toString() || "";
+		document.getElementById('usrfield4').value = usrFields[3].value.number.toString() || "";
+		document.getElementById('usrfield5').value = usrFields[5].value.number.toString() || "";
+		document.getElementById('usrfield6').value = usrFields[6].value.number.toString() || "";
+		document.getElementById('usrfield7').value = usrFields[7].value.number.toString() || "";
+		document.getElementById('usrfield8').value = usrFields[8].value.number.toString() || "";
 
 		document.getElementById('usrfield9').checked  = usrFields[9].value.boolean ;
 		document.getElementById('usrfield10').checked  = usrFields[10].value.boolean;
